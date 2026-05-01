cc776b312b871d97c9374bcf253d77c314004181
Leo.defossez <leo.defossez.etu@univ-lille.fr>
2026-05-01 18:23:47 +0200

    init project, front and back Create a basic structure for front and back

---
+++ project/back/src/test/java/com/puericulture/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.puericulture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Fail.fail;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		fail();
+	}
+
+}
